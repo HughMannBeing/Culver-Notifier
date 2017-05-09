1a96d845e8d8091b87a791568f96f587a220abee
HughMannBeing <airseahorse@gmail.com>
2017-05-08 21:59:51 -0600

    Add files via upload

--- Culvers Notifier/informMen.py
+++ Culvers Notifier/informMen.py
@@ -1,10 +1,18 @@
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 import updateUser
+from math import sqrt
 
 masterList = []
 specialCases = {}
 
+
+def getDistTo(testCords, newLocation):
+    #Getting the two coordinates becase they're together
+    latitude = str(testCords)[0:5]
+    longitude = str(testCords)[5:len(testCords)]
+    #newLocation might have to use something alittle more complicated, but we'll cross that brige later
+    return 100
 
 def createMail(newLocation):
     masterList = updateUser.updateList()
@@ -24,6 +32,15 @@ def createMail(newLocation):
         else:
             #Pass, and loop through at the end
             pass
+    for key, value in specialCases:
+        msg = MIMEMultipart()
+        msg['From'] = fromaddr
+        msg['To'] = str(key)
+        msg['Subject'] = "A new Culver's has opened!"
+        body = "A new Culver's has opened in " + str(newLocation) + "! Thats only: " + str(getDistTo(value, newLocation) + " Miles from you!")
+        msg.attach(MIMEText(body, 'plain'))
+        text = msg.as_string()
+        server.sendmail(fromaddr, str(i), text)
     for i in masterList:
         msg = MIMEMultipart()
         msg['From'] = fromaddr
